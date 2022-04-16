600b7bf8f2d039f7614ddb4df32a75a557116218
jumtp <49558643+jumtp@users.noreply.github.com>
2022-04-16 21:31:26 +0800

    完成的效果

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,41 @@ interface IProps {
 
 }
 
+
+
 const App: FC<IProps> = (): ReactElement => {
+    const themes = ['theme-blue','theme-green']
+    const changeTheme = (theme:string)=>{
+        const body = document.body;
+        themes.forEach(e=>{
+            body.classList.remove(e)
+        })
+        body.classList.add(theme)
+    }
     return (
-        <div className={`bg-red-200 h-screen flex justify-center items-center`}>
-            <div className={`bg-red-300 rounded-2xl p-8 min-w-max min-h-max flex flex-col items-center`}>
-                <h1 className={`text-3xl font-extrabold`}>尝试定制你的主题！</h1>
-                <h2 className={`text-lg my-3 text-amber-50`}>我们将会使用Tailwindcss和CSS3变量制作主题切换</h2>
-                <div className={"w-full flex justify-evenly "}>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-red-400  hover:bg-red-500 `}
+        <div className={`transition-all duration-500 bg-skin-base h-screen flex justify-center items-center`}>
+            <div className={`transition-all duration-500 bg-skin-card rounded-2xl p-8 min-w-max min-h-max flex flex-col items-center`}>
+                <h1 className={`transition-all duration-500 text-amber-50 text-3xl font-extrabold`}>尝试定制你的主题！</h1>
+                <h2 className={`transition-all duration-500 text-lg my-3 text-amber-50`}>我们将会使用Tailwindcss和CSS3变量制作主题切换</h2>
+                <div className={"transition-all duration-500 w-full flex justify-evenly "}>
+                    <input className={`transition-all duration-500 transition-colors cursor-pointer p-2 rounded text-amber-50 opacity-90
+                           bg-skin-btn  hover:bg-skin-btn-hover `}
                            type="button"
-                           value="红色主题"/>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-blue-400  hover:bg-blue-500 `}
+                           value="红色主题"
+                           onClick={()=>changeTheme("")}
+                    />
+                    <input className={`transition-all duration-500 cursor-pointer p-2 rounded text-amber-50 opacity-90
+                           bg-skin-btn  hover:bg-skin-btn-hover `}
                            type="button"
-                           value="蓝色主题"/>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-green-400  hover:bg-green-500 `}
+                           value="蓝色主题"
+                           onClick={()=>changeTheme(themes[0])}
+                    />
+                    <input className={`transition-all duration-500 transition-colors cursor-pointer p-2 rounded text-amber-50 opacity-90
+                           bg-skin-btn  hover:bg-skin-btn-hover `}
                            type="button"
-                           value="绿色主题"/>
+                           value="绿色主题"
+                           onClick={()=>changeTheme(themes[1])}
+                    />
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+function withOpacity(variableName) {
+    return ({opacityValue}) => {
+        if (opacityValue != null) {
+            return `rgba(var(${variableName}),${opacityValue})`
+        }
+        return `rgb(var(${variableName})`
+    }
+}
+
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            backgroundColor: {
+                skin: {
+                    base: withOpacity(`--color-bg-l1`),
+                    card: withOpacity(`--color-bg-l2`),
+                    btn: withOpacity(`--color-bg-l3`),
+                    'btn-hover': withOpacity(`--color-bg-l4`),
+                }
+            }
+        },
+    },
+    plugins: [],
 }
