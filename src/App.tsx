82790a87cd59a23221881441675f963a15a62ddf
jumtp <49558643+jumtp@users.noreply.github.com>
2022-04-16 22:59:58 +0800

    一次简单的修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, ReactEventHandler} from 'react';
 
 interface IProps {
 
 }
 
 const App: FC<IProps> = (): ReactElement => {
+
+
+    const Btn: FC<{ value: string, onClick?: ReactEventHandler }> = ({value,onClick}) => {
+        return <input className={`transition-colors cursor-pointer p-2 rounded bg-red-400  hover:bg-red-500 `}
+                      type="button"
+                      value={value} onClick={onClick}/>
+    }
+
     return (
-        <div className={`bg-red-200 h-screen flex justify-center items-center`}>
-            <div className={`bg-red-300 rounded-2xl p-8 min-w-max min-h-max flex flex-col items-center`}>
-                <h1 className={`text-3xl font-extrabold`}>尝试定制你的主题！</h1>
-                <h2 className={`text-lg my-3 text-amber-50`}>我们将会使用Tailwindcss和CSS3变量制作主题切换</h2>
-                <div className={"w-full flex justify-evenly "}>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-red-400  hover:bg-red-500 `}
-                           type="button"
-                           value="红色主题"/>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-blue-400  hover:bg-blue-500 `}
-                           type="button"
-                           value="蓝色主题"/>
-                    <input className={`transition-colors cursor-pointer p-2 rounded bg-green-400  hover:bg-green-500 `}
-                           type="button"
-                           value="绿色主题"/>
+        //背景颜色
+        <div className={`transition-all bg-red-200 h-screen flex justify-center items-center`}>
+            {/* 卡片颜色 */}
+            <div className={`transition-all bg-red-300 rounded-2xl p-8 min-w-max min-h-max flex flex-col items-center`}>
+                <h1 className={`transition-all text-3xl font-extrabold`}>尝试定制你的主题！</h1>
+                <h2 className={`transition-all text-lg my-3 text-amber-50`}>我们将会使用Tailwindcss和CSS3变量制作主题切换</h2>
+                <div className={"transition-all w-full flex justify-evenly "}>
+                    <Btn value={"红色主题"} onClick={()=>{console.log('1')}}/>
+                    <Btn value={"蓝色主题"} onClick={()=>{console.log('2')}}/>
+                    <Btn value={"绿色主题"} onClick={()=>{console.log('3')}}/>
                 </div>
             </div>
         </div>
